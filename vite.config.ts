3ae87ea804040f69ca5415439a8098676cb543e2
mazikaru492 <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2025-10-11 17:33:30 +0900

    Update project files

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite' // ← この行を追加
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // ← この行を追加
-  ],
+  plugins: [react(), tailwindcss()],
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/'
 })
